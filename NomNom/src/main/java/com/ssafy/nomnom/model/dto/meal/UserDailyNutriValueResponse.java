2333922300c84fd67fed60f9dce3469b55464a6a
mindyso <mindyso7151@gmail.com>
2025-05-21 10:55:16 +0900

    250521_MealMapper_MealDao_complete

--- NomNom/src/main/java/com/ssafy/nomnom/model/dto/meal/UserDailyNutriValueResponse.java
+++ NomNom/src/main/java/com/ssafy/nomnom/model/dto/meal/UserDailyNutriValueResponse.java
@@ -4,24 +4,72 @@ public class UserDailyNutriValueResponse {
 	private double Energy;              // 칼로리 (Energy, kcal)
 	private double Carbohydrate;        // 탄수화물
 	private double Sugar;               // 당류
-	private double DietaryFiber;        // 식이섬유
 	private double Protein;             // 단백질
-	private double Ash;                 // 회분
 	private double TotalFattyAcids;     // 총 지방산
-	private double SaturatedFats;       // 포화지방산
-	private double UnsaturatedFats;     // 단일불포화지방산 + 다중불포화지방산
 	private double Water;               // 수분
-	private double VitaminACarotene;    // 비타민 A (총 카로틴계)
-	private double VitaminB1;           // 비타민 B1 (Thiamine)
-	private double VitaminB2;           // 비타민 B2 (Riboflavin)
-	private double Niacin;              // 비타민 B3 나이아신
-	private double Folate;              // 엽산 (Folate)
-	private double VitaminC;            // 비타민 C
-	private double VitaminD;            // 비타민 D
-	private double Cholesterol;         // 콜레스테롤
 	private double Sodium;              // 나트륨
-	private double Calcium;             // 칼슘
-	private double Iron;                // 철
-	private double Phosphorus;          // 인
-	private double Potassium;           // 칼륨
+	
+	public UserDailyNutriValueResponse() {
+	}
+	public UserDailyNutriValueResponse(double energy, double carbohydrate, double sugar, double protein,
+			double totalFattyAcids, double water, double sodium) {
+		super();
+		Energy = energy;
+		Carbohydrate = carbohydrate;
+		Sugar = sugar;
+		Protein = protein;
+		TotalFattyAcids = totalFattyAcids;
+		Water = water;
+		Sodium = sodium;
+	}
+	public double getEnergy() {
+		return Energy;
+	}
+	public void setEnergy(double energy) {
+		Energy = energy;
+	}
+	public double getCarbohydrate() {
+		return Carbohydrate;
+	}
+	public void setCarbohydrate(double carbohydrate) {
+		Carbohydrate = carbohydrate;
+	}
+	public double getSugar() {
+		return Sugar;
+	}
+	public void setSugar(double sugar) {
+		Sugar = sugar;
+	}
+	public double getProtein() {
+		return Protein;
+	}
+	public void setProtein(double protein) {
+		Protein = protein;
+	}
+	public double getTotalFattyAcids() {
+		return TotalFattyAcids;
+	}
+	public void setTotalFattyAcids(double totalFattyAcids) {
+		TotalFattyAcids = totalFattyAcids;
+	}
+	public double getWater() {
+		return Water;
+	}
+	public void setWater(double water) {
+		Water = water;
+	}
+	public double getSodium() {
+		return Sodium;
+	}
+	public void setSodium(double sodium) {
+		Sodium = sodium;
+	}
+	@Override
+	public String toString() {
+		return "UserDailyNutriValueResponse [Energy=" + Energy + ", Carbohydrate=" + Carbohydrate + ", Sugar=" + Sugar
+				+ ", Protein=" + Protein + ", TotalFattyAcids=" + TotalFattyAcids + ", Water=" + Water + ", Sodium="
+				+ Sodium + "]";
+	}
+	
+	
 }
