87a1f90cbd6167547bf35e743f448f573e82dd66
MindySo <mindyso7151@gmail.com>
2025-05-21 21:08:37 +0900

    250521_folder_structure_updated_and_front_added
    Former-commit-id: 7304cb40b4dffe644ef922d1d5e9deef67445aff

---
+++ NomNom_front/src/components/challenge/ChallengeDetail.vue
@@ -0,0 +1,26 @@
+<template>
+<div>
+    챌린지 상세 : {{ id }}
+    <button class="btn" @click="requestChallengeList">목록</button>
+</div>
+</template>
+
+<script setup>
+import {ref} from 'vue';
+import {useRoute, useRouter} from 'vue-router';
+
+const router = useRouter();
+const route = useRoute();
+
+const regDate = route.params.regDate;
+const board = ref({})
+
+const requestChallengeList = () => {
+    router.push({name: 'challengeList'});
+};
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ NomNom_front/src/components/meal/MealDetail.vue
@@ -0,0 +1,24 @@
+<template>
+<div>
+    식단 상세 : {{ regDate }}
+    <button class="btn" @click="requestMealList()">목록</button>
+</div>
+</template>
+
+<script setup>
+import {ref} from 'vue';
+import {useRoute, useRouter} from 'vue-router';
+
+const router = useRouter();
+const route = useRoute();
+const regDate = route.params.regDate;
+
+const requestMealList = () => {
+    router.push({name: 'mealList'});
+};
+
+</script>
+
+<style scoped>
+
+</style>
