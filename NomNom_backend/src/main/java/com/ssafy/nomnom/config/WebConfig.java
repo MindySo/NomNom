e2e8ff5de40d4e3d8409dbb1a800dfcef2e57612
MindySo <mindyso7151@gmail.com>
2025-05-26 20:20:10 +0900

    250526_error_solving
    Former-commit-id: a75806b25f9f7ff180ba52b58f60a8a50623d470

--- NomNom_backend/src/main/java/com/ssafy/nomnom/config/WebConfig.java
+++ NomNom_backend/src/main/java/com/ssafy/nomnom/config/WebConfig.java
@@ -1,6 +1,7 @@
 package com.ssafy.nomnom.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 //import org.springframework.beans.factory.annotation.Autowired;
 //import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -18,4 +19,13 @@ public class WebConfig implements WebMvcConfigurer {
 //	public void addInterceptors(InterceptorRegistry registry) {
 //		registry.addInterceptor(adminInterceptor).addPathPatterns("/users");
 //	}
+	
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // 모든 경로에 대해
+                .allowedOrigins("http://localhost:5173") // Vue 앱 도메인
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 필요한 메서드 허용
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
 }
